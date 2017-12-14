0ea02408f9db5d4bdb5410e186d2733496ad11b6
Durand D'souza <durand1@gmail.com>
2017-12-14 06:49:28 +0000

    Updates to show baked

--- show_baked.js
+++ show_baked.js
@@ -5,12 +5,7 @@ var margin = {
         left: 50
     },
     width = 2000 - margin.left - margin.right,
-    height = 800 - margin.top - margin.bottom,
-
-    padding = 2,
-    maxRadius = 30,
-    minRadius = 2
-
+    height = 800 - margin.top - margin.bottom
 
 var svg = d3.select("body")
     .append("svg")
@@ -23,19 +18,18 @@ var wrapper = svg
     .attr("class", "wrapper")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-topic_name = "european union"
-var topics = [topic_name]
+var topic_name = "european union"
 // var topics = ["Male", "Female"]
-var x = d3.scalePoint()
-    .domain(topics)
-    .range([0 + width / 4, width * 3 / 4])
+var x = d3.scaleLinear()
+    .domain([-350, 350])
+    .range([0, width])
 
 var xAxis = d3.axisBottom(x)
+
 var y = d3.scaleLinear()
     .domain([0, 0.3])
     .range([height, 0])
 
-var yAxis = d3.axisLeft(y)
 
 var tip = d3.tip()
     .attr("class", "d3-tip")
@@ -44,7 +38,7 @@ var tip = d3.tip()
         return `<div id="tooltip">
                     <h1 style="background-color: black;">${d.full_name}</h1>
                     <div class="mp-image-parent">
-                    <img class="mp-image" src="./mp-images/mp-${d.id}.jpg"/>
+                    <img class="mp-image" src="./mp-images/mp-${d.id}.jpg" style="opacity: 1;"/>
                     </div>
 <p>${d[topic_name].toFixed(2)}</p><p>
 </div>`
@@ -75,20 +69,19 @@ wrapper.append("text")
 var colorScale = d3.scaleOrdinal(d3.schemeCategory10)
     .domain(["Labour", "Conservative", "Liberal Democrat", "Scottish National Party"])
 
-wrapper.append("g")
-    .attr("class", "x-axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis)
+// wrapper.append("g")
+//     .attr("class", "x-axis")
+//     .attr("transform", "translate(0," + height + ")")
+//     .call(xAxis)
 
-// Set default positions for nodes
-function getRandomInt(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min)) + min //The maximum is exclusive and the minimum is inclusive
-}
+var yAxis = d3.axisLeft(y)
+// wrapper.append("g")
+//     .attr("class", "y-axis")
+//     .attr("transform", "translate(" + x(0)+ ", 0)")
+//     .call(yAxis)
 
-baked_positions = []
-nodes = []
+var baked_positions = []
+var nodes = []
 
 d3.queue()
     .defer(d3.csv,
@@ -101,7 +94,7 @@ d3.queue()
     .await(
         function (error, baked_data, mp_data, topic_medians_data) {
 
-            topic_medians = {}
+            var topic_medians = {}
             topic_medians_data.forEach(a => { topic_medians[a.topic] = { male: a.male, female: a.female } })
 
             baked_data.forEach(function (row) {
@@ -110,7 +103,7 @@ d3.queue()
                     .forEach(
                         function (colname) {
                             if (colname == "id" || colname.slice(-1) == "y") return
-                            topic = colname.slice(0, -2)
+                            var topic = colname.slice(0, -2)
                             baked_positions.push({
                                 "id": +row["id"],
 
@@ -128,7 +121,7 @@ d3.queue()
 
             // Convert wide data to long
             nodes = mp_data.map(function (d) {
-                node = {
+                var node = {
                     "id": +d.id,
                     "full_name": d.full_name,
                     "party": d.Party,
@@ -157,14 +150,14 @@ d3.queue()
                 baked_data = baked_positions.filter(d => d.key == topic_name)[0].values
 
                 nodes_male.map(function (d) {
-                    n = baked_data.filter(n => n.id == d.id)[0]
-                    d.x = n.x * 2.5 + x("european union")
+                    var n = baked_data.filter(n => n.id == d.id)[0]
+                    d.x = x(n.x)-25
                     d.y = y(n.y)
                 })
 
                 nodes_female.map(function (d) {
-                    n = baked_data.filter(n => n.id == d.id)[0]
-                    d.x = n.x * 2.5 + x("european union")
+                    var n = baked_data.filter(n => n.id == d.id)[0]
+                    d.x = x(n.x)+25
                     d.y = y(n.y)
                 })
 
@@ -173,19 +166,14 @@ d3.queue()
                     .duration(1000)
 
                 // JOIN
-                var circle_male = wrapper.selectAll(".male-node")
+                circle_male = wrapper.selectAll(".male-node")
                     .data(nodes_male)
 
                 // UPDATE
                 circle_male
                     .transition(transition)
-                    .attr("cx", function (d) {
-                        return d.x
-                    })
-                    .attr("cy", function (d) {
-                        // return Math.max(0 + 2.5, Math.min(height - 2.5, d.y))
-                        return d.y
-                    })
+                    .attr("cx", d => d.x)
+                    .attr("cy", d => d.y)
 
                 // ENTER
                 circle_male
@@ -193,33 +181,28 @@ d3.queue()
                     .append("circle")
                     .attr("class", "male-node")
                     .style("fill", "red")
-                    .attr("cx", function (d) {
-                        return d.x
-                    })
-                    .attr("cy", function (d) {
-                        // return Math.max(0 + 2.5, Math.min(height - 2.5, d.y))
-                        return d.y
-                    })
                     .attr("r", 1.8)
-                    .style("opacity", 0.7)
                     .on("mouseover", tip.show)
                     .on("mouseout", tip.hide)
+                    .attr("cx", x(0))
+                    .attr("cy", d => d.y)
+                    .style("opacity", 0.0)
+                    .transition(transition)
+                    .delay((d, i) => 100*Math.sqrt(i))
+                    .style("opacity", 0.7)
+                    .attr("cx", d => d.x)
+                    .attr("cy", d => d.y)
 
                 // Female nodes
                 // JOIN
-                var circle_female = wrapper.selectAll(".female-node")
+                circle_female = wrapper.selectAll(".female-node")
                     .data(nodes_female)
 
                 // UPDATE
                 circle_female
                     .transition(transition)
-                    .attr("cx", function (d) {
-                        return d.x
-                    })
-                    .attr("cy", function (d) {
-                        // return Math.max(0 + 2.5, Math.min(height - 2.5, d.y))
-                        return d.y
-                    })
+                    .attr("cx", d => d.x)
+                    .attr("cy", d => d.y)
 
                 // ENTER
                 circle_female
@@ -227,21 +210,23 @@ d3.queue()
                     .append("circle")
                     .attr("class", "female-node")
                     .style("fill", "lightblue")
-                    .attr("cx", function (d) {
-                        return d.x
-                    })
-                    .attr("cy", function (d) {
-                        // return Math.max(0 + 2.5, Math.min(height - 2.5, d.y))
-                        return d.y
-                    })
+                    .attr("cx", d => d.x)
+                    .attr("cy", d => d.y)
                     .attr("r", 1.8)
-                    .style("opacity", 0.7)
                     .on("mouseover", tip.show)
                     .on("mouseout", tip.hide)
+                    .attr("cx", x(0))
+                    .attr("cy", d => d.y)
+                    .style("opacity", 0.0)
+                    .transition(transition)
+                    .delay((d, i) => 100*Math.sqrt(i))
+                    .style("opacity", 0.7)
+                    .attr("cx", d => d.x)
+                    .attr("cy", d => d.y)
 
 
-                var min_male_hist = Math.min(...nodes_male.map(d=>d.x))
-                var max_female_hist = Math.max(...nodes_female.map(d=>d.x))
+                var min_male_hist = Math.min(...nodes_male.map(d => d.x))
+                var max_female_hist = Math.max(...nodes_female.map(d => d.x))
 
                 // Male median fraction
                 // Join
@@ -262,6 +247,10 @@ d3.queue()
                     .enter()
                     .append("line")
                     .attr("class", "male-median")
+                    .attr("y1", y(0))
+                    .attr("y2", y(0))
+                    .transition(transition)
+                    .delay(2000)
                     .attr("x1", min_male_hist - 10)
                     .attr("x2", min_male_hist - 50)
                     .attr("y1", d => y(d))
@@ -290,6 +279,10 @@ d3.queue()
                     .attr("class", "female-median")
                     .attr("x1", max_female_hist + 10)
                     .attr("x2", max_female_hist + 50)
+                    .attr("y1", y(0))
+                    .attr("y2", y(0))
+                    .transition(transition)
+                    .delay(2000)
                     .attr("y1", d => y(d))
                     .attr("y2", d => y(d))
                     .style("stroke-width", 2)
@@ -309,10 +302,8 @@ d3.queue()
             // Call function initially
             update()
 
-            simulation_male = d3.forceSimulation(nodes_male)
-                .force("x", d3.forceX(function (d) {
-                    return x(topic_name)
-                })
+            var simulation_male = d3.forceSimulation(nodes_male)
+                .force("x", d3.forceX(x(0))
                     .strength(0.1))
                 .force("y", d3.forceY(function (d) {
                     return y(d[topic_name])
@@ -325,10 +316,8 @@ d3.queue()
                 .alphaMin(0.000000001)
                 .velocityDecay(0.1)
 
-            simulation_female = d3.forceSimulation(nodes_female)
-                .force("x", d3.forceX(function (d) {
-                    return x(topic_name)
-                })
+            var simulation_female = d3.forceSimulation(nodes_female)
+                .force("x", d3.forceX(x(0))
                     .strength(0.1))
                 .force("collide",
                     d3.forceCollide(7.0)
@@ -339,8 +328,8 @@ d3.queue()
 
             simulation_male.on("tick", function () {
                 nodes_male.map(function (d) {
-                    d.x = Math.min(d.x, x(topic_name) - 2.5)
-                    node_y = y(d[topic_name])
+                    d.x = Math.min(d.x, x(0) - 2.5)
+                    var node_y = y(d[topic_name])
                     d.y = Math.min(node_y + 5, Math.max(node_y - 5, d.y))
                     d.y = Math.min(y(0), d.y)
                 })
@@ -356,21 +345,20 @@ d3.queue()
 
             simulation_female.on("tick", function () {
                 nodes_female.map(function (d) {
-                    d.x = Math.max(x(topic_name) + 2.5, d.x)
-                    node_y = y(d[topic_name])
+                    d.x = Math.max(x(0) + 2.5, d.x)
+                    var node_y = y(d[topic_name])
                     d.y = Math.min(node_y + 2.5, Math.max(node_y - 2.5, d.y))
                 })
                 circle_female.attr("cx", function (d) {
                     return d.x
                 })
                     .attr("cy", function (d) {
-                        // return Math.max(0 + 2.5, Math.min(height - 2.5, d.y))
                         return d.y
                     })
             })
                 .stop()
 
-            // for (var i = 0; i < 300; ++i) simulation.tick()
+            // for (var i = 0; i < 30; ++i) simulation_male.tick()
 
 
         })
